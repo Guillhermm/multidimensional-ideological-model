c62df616a209647afe4d67fc44ea3358a155ae09
Guillhermm <guillhermm_zenni@hotmail.com>
2026-02-15 16:45:44 -0300

    add keyboard support for interactions

--- scripts/interaction.js
+++ scripts/interaction.js
@@ -21,3 +21,40 @@ canvas.addEventListener('wheel', e => {
 
   setZoom(e.deltaY > 0 ? 0.95 : 1.05);
 });
+
+window.addEventListener('keydown', e => {
+  // For rotation only.
+  const step = 15;
+
+  switch (e.key) {
+    case 'ArrowLeft':
+      setRotation(-step, 0);
+      break;
+
+    case 'ArrowRight':
+      setRotation(step, 0);
+      break;
+
+    case 'ArrowUp':
+      setRotation(0, -step);
+      break;
+
+    case 'ArrowDown':
+      setRotation(0, step);
+      break;
+
+    case '+':
+    case '=':
+      setZoom(1.1);
+      break;
+
+    case '-':
+    case '_':
+      setZoom(0.9);
+      break;
+
+    case '0':
+      zoom = 1;
+      break;
+  }
+});
